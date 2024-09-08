96cd66e3128a6e5a4d0ddd8f18447e3c97cb270e
kaytee07 <kofi.taylor07@gmail.com>
2024-09-08 07:27:53 +0000

    add

--- components/PackagePopular.tsx
+++ components/PackagePopular.tsx
@@ -25,7 +25,25 @@ const PackagePopular = () => {
                         </div>
                         </div>
                         <div className="flex justify-between">
-                            <svg xmlns="http://www.w3.org/2000/svg" color="#ff8041" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+                            <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    color="#ff8041"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2" // Changed from stroke-width
+                                    strokeLinecap="round" // Changed from stroke-linecap
+                                    strokeLinejoin="round" // Changed from stroke-linejoin
+                                    className="lucide lucide-calendar" // Changed class to className
+                                    >
+                                    <path d="M8 2v4"/>
+                                    <path d="M16 2v4"/>
+                                    <rect width="18" height="18" x="3" y="4" rx="2"/>
+                                    <path d="M3 10h18"/>
+                            </svg>
+
                             <p className="block font-sans text-sm antialiased font-normal leading-normal text-[#ff8041] opacity-75">
                                 January 7 - January 9
                             </p>
