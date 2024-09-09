ec29376b4b913c64c5838ff1c6032d746e9bdcab
kaytee07 <kofi.taylor07@gmail.com>
2024-09-09 14:24:28 +0000

    sign in sign up buttons implemented

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 const Hero = () => {
   return (
@@ -6,21 +7,20 @@ const Hero = () => {
     <div
     className="
     bg-[url('https://images.unsplash.com/photo-1472938714740-c788a1dbfa12?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] 
-     bg-cover bg-center relative"
+     bg-cover bg-center flex flex-col justify-center gap-3"
     style={{
         width: "100%",
         height: "30rem"
     }} 
     >
-       <div className="absolute flex items-center justify-center inset-0 rounded-md bg-black opacity-30"
-       style={{
-        width: "100%",
-        height: "30rem"
-       }}>
+       
          <h1 className="text-center scroll-m-20 text-4xl text-white font-bold tracking-tight lg:text-4xl">
-        Find The Next Destination In Ghana  
+          A Trip To Ghana  
         </h1>
-       </div>
+        <div className="buttons flex  justify-center gap-2">
+          <Button className="bg-[#ff8041]">Sign in</Button>
+          <Button className="bg-[#ff8041]">Sign up</Button>
+        </div>
     </div>
     </div>
   )
